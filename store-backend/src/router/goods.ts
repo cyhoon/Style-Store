446507273d378f87f6397858f1c15803538a42af
ihello72 <ihello0720@gmail.com>
2018-11-20 09:50:38 +0900

    Update checkToken scope  in api /goods

--- store-backend/src/router/goods.ts
+++ store-backend/src/router/goods.ts
@@ -1,10 +1,11 @@
 import * as Router from "koa-router";
 import { getGoods, getDetailGoods, saveGoods } from "../controller/goodsCtrl";
+import { checkToken } from "../middlewares/checkToken";
 
 const goodsRouter: Router = new Router();
 
 goodsRouter.get('/', getGoods);
 goodsRouter.get('/:goodsId', getDetailGoods);
-goodsRouter.post('/', saveGoods);
+goodsRouter.post('/', checkToken, saveGoods);
 
 export default goodsRouter;
